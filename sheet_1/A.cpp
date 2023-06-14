3a42c734c734276cf627452946cc59c4ad49e0c0
Mohamed A. Raslan <112321568+Raslan-11@users.noreply.github.com>
2023-06-14 14:33:36 +0300

    Add files via upload

---
+++ sheet_1/A.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main()
+{
+    string word;
+    cin>> word;
+    cout << "Hello, "+word<< endl;
+    return 0;
+}
---
+++ sheet_1/B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+ 
+using namespace std;
+ 
+int main()
+{
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout <<a<<"\n"<<b<<"\n"<<c<<"\n"<<d<<"\n"<<e<< endl;
+    return 0;
+}
